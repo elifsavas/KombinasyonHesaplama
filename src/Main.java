df246d2ede177fb057b0f1088a1e941a7b387759
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-10-31 17:26:19 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        long n, r;
+        long i;
+        long total1 = 1, total2 = 1, total3 = 1;
+
+        System.out.println("(n,r) Yapıdaki Kombinasyon için");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("'n' Değerini Giriniz:");
+        n = input.nextLong();
+
+        System.out.print("'r' Değerini Giriniz: ");
+        r = input.nextInt();
+
+        if (n > 1) {
+            for (i = 1; i <= n; i++) {
+
+                total1 = total1 * i;
+
+            }
+
+            for (i = 1; i <= r; i++) {
+
+                total2 = total2 * i;
+
+            }
+
+            for (i = 1; i <= (n - r); i++) {
+
+                total3 = total3 * i;
+            }
+
+            System.out.print(n + " ve " + r + " Kombinasyonu: ");
+            System.out.println(total1 / (total2 * total3));
+
+        } else {
+            System.out.println("Hatalı Giriş Yaptınız");
+        }
+
+
+
     }
 }
